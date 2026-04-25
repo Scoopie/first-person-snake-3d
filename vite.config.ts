760dac86d8397f0d2ac0e78669059e4a05ddf7d2
Kelby Rogers <mail.git@muntedpossum.com>
2026-04-25 13:54:45 +1200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        legacy: "first_person_snake_3_d.html"
+      }
+    }
+  },
   server: {
     host: "127.0.0.1",
     port: 51920,
